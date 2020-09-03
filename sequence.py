e50b1b960600b428d5e40d5204eea74ca1a38938
Björn Andri Pálsson <bjornandri@Bjorns-MacBook-Pro.local>
2020-09-03 15:21:21 +0000

    init

--- sequence.py
+++ sequence.py
@@ -2,10 +2,9 @@ n = int(input("Enter the length of the sequence: ")) # Do not change this line
 num1=0
 num2=1
 num3=2
-print(num1)
 print(num2)
 print(num3)
-for i in range(n):
+for i in range(n-2):
     next_num= num1 + num2 + num3
     print(next_num)
     num1 = num2
